cca1349cd88c0b6a251228218c4df94862029949
Doğukan Arslan <dogukan2arslan@hotmail.com>
2020-01-05 23:27:26 +0300

    Updates

---
+++ precache-manifest.6ff1fe70b140ae2d9a13681a8d2ef6ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b015c7dca4d2f1914029bde36dfaebcf",
+    "url": "/cocktails-menu-app/index.html"
+  },
+  {
+    "revision": "dcef3dcd5217b4a8ff74",
+    "url": "/cocktails-menu-app/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "f5c5ec1da97d64e8c4a1",
+    "url": "/cocktails-menu-app/static/css/main.9f6ab6e5.chunk.css"
+  },
+  {
+    "revision": "dcef3dcd5217b4a8ff74",
+    "url": "/cocktails-menu-app/static/js/2.f50b4562.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/cocktails-menu-app/static/js/2.f50b4562.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5c5ec1da97d64e8c4a1",
+    "url": "/cocktails-menu-app/static/js/main.e6a7eead.chunk.js"
+  },
+  {
+    "revision": "143e7f1feb834d143b21",
+    "url": "/cocktails-menu-app/static/js/runtime-main.4856c904.js"
+  }
+]);
